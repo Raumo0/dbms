feab99e8e54d1750dede7648e6aa546e90ca3418
Daniil Harapko <dharapko@gmail.com>
2017-03-19 18:21:23 +0300

    Added test for select. Removed main.

---
+++ dao/src/test/java/by/bsuir/dbms/dao/Impl/SelectImplTest.java
@@ -0,0 +1,55 @@
+package by.bsuir.dbms.dao.Impl;
+
+import by.bsuir.dbms.constants.DAOConstants;
+import by.bsuir.dbms.dao.Select;
+import by.bsuir.dbms.tools.FileWorker;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SelectImplTest {
+    private static String assertMsg;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        assertMsg = "() method from SelectImplTest failed: ";
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void selectAllFromTable() throws Exception {
+        String msg = Thread.currentThread().getStackTrace()[1].getMethodName() + assertMsg;
+
+        String table = DAOConstants.PATH_DB_TEST + "select.csv";
+        FileWorker fileWorker = new FileWorker(table);
+        List<String[]> expected = new ArrayList<>();
+        List<String[]> actual;
+        Select select = SelectImpl.getInstance();
+
+        expected.add(new String[]{"id", "title", "page_count"});//columns
+        expected.add(new String[]{"1", "Coombe", "319"});//values
+        expected.add(new String[]{"2", "My land", "543"});//values
+        expected.add(new String[]{"3", "Disoptico", "1306"});//values
+        fileWorker.writeCSV(expected);
+        actual = select.selectAllFromTable(table);
+
+        Assert.assertEquals(msg, expected.size(), actual.size());
+        Assert.assertArrayEquals(msg, expected.get(0), actual.get(0));
+        Assert.assertArrayEquals(msg, expected.get(1), actual.get(1));
+        Assert.assertArrayEquals(msg, expected.get(2), actual.get(2));
+        Assert.assertArrayEquals(msg, expected.get(3), actual.get(3));
+    }
+
+}
